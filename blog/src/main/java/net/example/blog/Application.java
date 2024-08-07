5e8c77d63bcd5eb7878959572c04ff43779c2280
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-08-07 09:59:51 -0400

    code clean up

--- blog/src/main/java/net/example/blog/Application.java
+++ blog/src/main/java/net/example/blog/Application.java
@@ -14,15 +14,4 @@ public class Application {
 
 		SpringApplication.run(Application.class, args);
 	}
-
-
-	//this is how will insert some data.
-//	@Bean
-//	CommandLineRunner runner(RunRepository repository){
-//		return args -> {
-//			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR);
-//			logger.info("Run: \n"+ run);
-//			repository.create(run);
-//		};
-//	}
 }
